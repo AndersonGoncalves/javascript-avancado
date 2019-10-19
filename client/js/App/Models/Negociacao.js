40b1e2045a9d6a936d2f606e04c3da4131e0ef84
Anderson Gonçalves <anndersonn.gonncalves@gmail.com>
2019-10-19 00:18:33 -0300

    Lidar com data é trabalhoso? Chame um ajudante!

--- client/js/App/Models/Negociacao.js
+++ client/js/App/Models/Negociacao.js
@@ -1,27 +1,30 @@
 class Negociacao {
-
+    
     constructor(data, quantidade, valor) {
-
+        
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-
-        Object.freeze(this); //Evita que as propriedades do objeto seja alteradas após a instância
+        Object.freeze(this);
     }
-
+    
     get volume() {
+        
         return this._quantidade * this._valor;
     }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
+    
     get data() {
+        
         return new Date(this._data.getTime());
     }
-
+    
+    get quantidade() {
+        
+        return this._quantidade;
+    }
+    
     get valor() {
+        
         return this._valor;
     }
 }
